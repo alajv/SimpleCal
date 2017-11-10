2d881fd3f10b4c5e5b492db8dffec21c26c1937c
alajv <williamlidholm1@gmail.com>
2017-11-10 12:03:32 +0100

    Lista med dummy datum

---
+++ app/src/main/java/com/a2/william/simplecal/DateListHandler.java
@@ -0,0 +1,34 @@
+package com.a2.william.simplecal;
+
+import java.util.ArrayList;
+
+/**
+ * Created by William on 2017-11-10.
+ */
+
+public class DateListHandler {
+
+    public static void Main(String[]args) {
+
+
+    }
+
+    public ArrayList<String> getList(){
+
+        ArrayList<String> dummyDate = new ArrayList<String>();
+
+        dummyDate.add("Januari 1");
+        dummyDate.add("Januari 2");
+        dummyDate.add("Januari 3");
+        dummyDate.add("Januari 4");
+        dummyDate.add("Januari 5");
+        dummyDate.add("Januari 6");
+        dummyDate.add("Januari 7");
+        dummyDate.add("Januari 8");
+        dummyDate.add("Januari 9");
+        dummyDate.add("Januari 10");
+        dummyDate.add("Januari 11");
+
+        return dummyDate;
+    }
+}
--- app/src/main/java/com/a2/william/simplecal/MainActivity.java
+++ app/src/main/java/com/a2/william/simplecal/MainActivity.java
@@ -1,13 +1,33 @@
 package com.a2.william.simplecal;
 
+import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+        ListAdapter adapter;
+        DateListHandler dlh = new DateListHandler();
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.DateList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dlh.getList());
+
+
+        listView.setAdapter(adapter);
     }
 }
